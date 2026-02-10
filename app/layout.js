3b574346cc250cac0ab0e9db46873e74436a08cb
MoulanaRimaz <rimaz.m@virtunova.io>
2026-02-10 10:14:23 +0530

    Add AIMSS logo as favicon

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,9 @@ const roboto = Roboto({ weight: ["400", "700"], subsets: ["latin"], variable: "-
 export const metadata = {
   title: "AIMSS@Z | Zahira National School Hambantota",
   description: "Association for Improvement of Mathematics and Science Streams at Zahira National School Hambantota.",
+  icons: {
+    icon: '/icon.jpg',
+  },
 };
 
 export default function RootLayout({ children }) {
